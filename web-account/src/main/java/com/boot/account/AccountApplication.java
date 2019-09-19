03f4f5a83dc3eecfb2ad7a198e01f7b4376cc830
zhangwei <zhangwei.163.com>
2019-09-19 17:27:14 +0800

    分支合并

--- web-account/src/main/java/com/boot/account/AccountApplication.java
+++ web-account/src/main/java/com/boot/account/AccountApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * restful 封装统一的结果集
+ * @author zhangwei
  */
 @SpringBootApplication
 public class AccountApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
     }
